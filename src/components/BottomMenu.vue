997e1e42f6e8e1ebdda07fb2807f20f4a53c31c0
Vidhyanand CS <vithushaji@gmail.com>
2022-10-26 19:57:42 +0530

    bottom menu fixed

--- src/components/BottomMenu.vue
+++ src/components/BottomMenu.vue
@@ -1,38 +1,85 @@
 <template>
   <!-- App Bottom Menu -->
   <div class="appBottomMenu">
-    <router-link to="/" class="item active">
+    <router-link
+      @click="selection('wallet')"
+      to="/"
+      :class="selectedMenu == 'wallet' ? 'item active' : 'item'"
+    >
       <div class="col">
-        <img src="../assets/icons/wallet-outline.svg" width="22" alt="" />
+        <img src="../assets/icons/wallet-outline.svg" width="22" alt="wallet" />
         <strong>Wallet</strong>
       </div>
     </router-link>
-    <router-link :to="{ name: 'portfolio' }" class="item">
+    <router-link
+      @click="selection('portfolio')"
+      :to="{ name: 'portfolio' }"
+      :class="selectedMenu == 'portfolio' ? 'item active' : 'item'"
+    >
       <div class="col">
-        <img src="../assets/icons/briefcase-outline.svg" width="22" alt="" />
+        <img
+          src="../assets/icons/briefcase-outline.svg"
+          width="22"
+          alt="portfolio"
+        />
         <strong>Portfolio</strong>
       </div>
     </router-link>
-    <router-link :to="{ name: 'channels' }" class="item">
+    <router-link
+      @click="selection('channels')"
+      :to="{ name: 'channels' }"
+      :class="selectedMenu == 'channels' ? 'item active' : 'item'"
+    >
       <div class="col">
-        <img src="../assets/icons/chatbubbles-outline.svg" width="22" alt="" />
+        <img
+          src="../assets/icons/chatbubbles-outline.svg"
+          width="22"
+          alt="channels"
+        />
         <strong>Channels</strong>
       </div>
     </router-link>
-    <router-link :to="{ name: 'transactions' }" class="item">
+    <router-link
+      @click="selection('transactions')"
+      :to="{ name: 'transactions' }"
+      :class="selectedMenu == 'transactions' ? 'item active' : 'item'"
+    >
       <div class="col">
-        <img src="../assets/icons/analytics-outline.svg" width="22" alt="" />
+        <img
+          src="../assets/icons/analytics-outline.svg"
+          width="22"
+          alt="transactions"
+        />
         <strong>Transactions</strong>
       </div>
     </router-link>
-    <router-link :to="{ name: 'settings' }" class="item">
+    <router-link
+      @click="selection('settings')"
+      :to="{ name: 'settings' }"
+      :class="selectedMenu == 'settings' ? 'item active' : 'item'"
+    >
       <div class="col">
-        <img src="../assets/icons/settings-outline.svg" width="22" alt="" />
+        <img
+          src="../assets/icons/settings-outline.svg"
+          width="22"
+          alt="settings"
+        />
         <strong>Settings</strong>
       </div>
     </router-link>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      selectedMenu: "wallet",
+    };
+  },
+  methods: {
+    selection(item) {
+      this.selectedMenu = item;
+    },
+  },
+};
 </script>
